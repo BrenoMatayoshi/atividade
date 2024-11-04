c064ec28d8b36b4655d4de3d0cf492ee2d7dcf82
Breno Masato Matayoshi <145488931+BrenoMatayoshi@users.noreply.github.com>
2024-11-04 14:57:29 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
   const checkbox = document.getElementById('menu-hamburguer');
-  checkbox.checked = !checkbox.checked; // Alterna o estado do checkbox
+  checkbox.checked = !checkbox.checked;
 }
